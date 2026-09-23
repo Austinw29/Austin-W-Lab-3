fe0df2e19ed280081178a8d98b38d31e22a4a1b5
Austinw29 <awalsh29@setoncatholic.org>
2026-09-23 18:23:14 +0000

    save 2

---
+++ main.py
@@ -0,0 +1,35 @@
+# Function that adds 2 numbers
+def add(x,y):
+    print(x+y)
+
+x= int(input("Enter the first number"))
+y = int(input("Enter the second number"))
+
+add(x,y)
+
+# Function that subtracts 2 numbers
+def sub(x,y):
+    print(x-y)
+
+x = int(input("Enter the first number"))
+y = int(input("Enter the second number"))
+
+sub(x,y)
+
+# Function that multiply 2 numbers
+def mult(x,y):
+    print(x*y)
+
+x = int(input("Enter the first number"))
+y = int(input("Enter the second number"))
+
+mult(x,y)
+
+# Function that divides 2 numbers
+def divi(x,y):
+    print(x/y)
+
+x = int(input("Enter the first number"))
+y = int(input("Enter the second number"))
+
+divi(x,y)
