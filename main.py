5191242c9ec5f83567b8659593cf1d7346aded6b
Austinw29 <awalsh29@setoncatholic.org>
2026-09-24 21:09:29 +0000

    save 3

--- main.py
+++ main.py
@@ -2,34 +2,45 @@
 def add(x,y):
     print(x+y)
 
-x= int(input("Enter the first number"))
-y = int(input("Enter the second number"))
-
-add(x,y)
-
 # Function that subtracts 2 numbers
 def sub(x,y):
     print(x-y)
-
-x = int(input("Enter the first number"))
-y = int(input("Enter the second number"))
-
-sub(x,y)
 
 # Function that multiply 2 numbers
 def mult(x,y):
     print(x*y)
 
-x = int(input("Enter the first number"))
-y = int(input("Enter the second number"))
-
-mult(x,y)
-
 # Function that divides 2 numbers
 def divi(x,y):
     print(x/y)
 
+##########################################################################################
+print("Welcome to Calculator Choom")
+print("Go Ahead and give it a try, its pretty nova")
+print(" whant to (a)dd (s)ubtract (m)ultiply (d)ivide (q)uit")
+
+user_input = input(":")
+#print(user_input)
 x = int(input("Enter the first number"))
 y = int(input("Enter the second number"))
 
-divi(x,y)
+while(True):
+
+    if user_input == 'a':
+    add(x,y)
+
+    elif user_input == 's':
+    sub(x,y)
+
+    elif user_input == 'm':
+    mult(x,y)
+
+    elif user_input == 'd':
+    divi(x,y)
+
+    elif user_input == 'q':
+    break
+
+    print("YOU GONK!!")
+
+
